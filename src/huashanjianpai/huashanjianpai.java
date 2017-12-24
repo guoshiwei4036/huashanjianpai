9150debd63134ede1712d08a5a643d0ce910054f
chenlei1379 <chenlei239@163.com>
2017-12-24 20:42:48 +0800

    提交提交test

--- src/huashanjianpai/huashanjianpai.java
+++ src/huashanjianpai/huashanjianpai.java
@@ -3,5 +3,6 @@ package huashanjianpai;
 public class huashanjianpai {
 	public static void main(String[] args) {
 		System.out.println("huashanjianpai!!!");
+		System.out.println("222222222222");
 	}
 }
